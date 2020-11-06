9214cfbb4a9557930b88a7a3c671f370064eeb50
Martin Kosík <martin.kosiik@gmail.com>
2020-11-06 21:49:11 +0100

    check for missing values

--- r_code.R
+++ r_code.R
@@ -3,9 +3,16 @@ setwd("C:/Users/marti/OneDrive/Plocha/skola/cerge/labor_econ/labor-econ-empirica
 library(haven)
 library(readxl)
 library(tidyverse)
+library(janitor)
 
 main_data<- read_dta("RQdata.dta")
 ur95 <- read_excel("ur95.xls")
 
 main_data <- main_data %>% 
   left_join(ur95, by = "code") 
+
+# Check number of missing values for each column
+main_data %>% 
+  map_int(~ sum(is.na(.)))
+
+# Variable c14 has 101 missing values, c13 22, and c06 18
