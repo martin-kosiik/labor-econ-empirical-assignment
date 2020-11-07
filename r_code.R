667022920147bedc8531751dc7b4e923bb212657
Martin Kosík <martin.kosiik@gmail.com>
2020-11-07 18:07:37 +0100

    latex regression tables

--- r_code.R
+++ r_code.R
@@ -1,10 +1,13 @@
+dir_path <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(dir_path)
 
-setwd("C:/Users/marti/OneDrive/Plocha/skola/cerge/labor_econ/labor-econ-empirical-assignment")
 library(haven)
 library(readxl)
 library(tidyverse)
-library(janitor)
+#library(janitor)
 library(estimatr)
+library(texreg)
+library(forcats)
 
 main_data<- read_dta("RQdata.dta", ) %>% as_factor()
 ur95 <- read_excel("ur95.xls")
@@ -16,7 +19,14 @@ main_data <- main_data %>%
          exper_sq = exper^2,
          has_child = (e03 > 0)*1,
          more_than_one_child = (e03 > 1)*1,
-         prague = (code == 3100) * 1)  %>% 
+         prague = (code == 3100) * 1,
+         edu_level = fct_recode(as.factor(a05), 'No school education' = '7',
+                                                'Primary' = '1',
+                                                'Apprenticeship (2 years)' = '2',
+                                                'Apprenticeship (3 - 4 years)' = '3',
+                                                'Secondary vocational with GCE' = '4',
+                                                'Grammar school with GCE' = '5',
+                                                'Higher education' = '6'))  %>% 
   rename(schooling_years = a09)
 
 # Check number of missing values for each column
@@ -24,7 +34,7 @@ main_data %>%
   map_int(~ sum(is.na(.)))
 
 # Variable c14 has 101 missing values, c13 22, and c06 18
-
+main_data
 
 # drop the missing data 
 main_data <- main_data %>% 
@@ -44,10 +54,14 @@ lm_robust(ln_y ~ as.factor(a05) + exper + exper_sq, full_time_men)
 
 # 7)
 # a)
-lm_robust(ln_y ~ has_child + as.factor(a05) + exper + exper_sq, full_time_men)
+one_child <- lm_robust(ln_y ~ has_child + as.factor(a05) + exper + exper_sq, full_time_men)
 
 # b)
-lm_robust(ln_y ~ I(more_than_one_child - has_child)+ as.factor(a05) + exper + exper_sq, full_time_men)
+one_vs_more <- lm_robust(ln_y ~ I(more_than_one_child - has_child)+ as.factor(a05) + exper + exper_sq, full_time_men)
+texreg(list(one_child, one_vs_more), caption = 'Effects of children', file = 'tables/children_effects.tex') #%>% 
+  cat() %>% 
+  write_file('tables/children_effects.tex')
+  
 
 # 8)
 # a, b)
